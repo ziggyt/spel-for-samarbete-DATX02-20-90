8ac610827bf4fa54542f87774a0b765b470fa787
Johan Lindkvist <johanlindkvist89@gmail.com>
2020-04-10 15:57:22 +0200

    Add landing pads only accept shown color

---
+++ Assets/Scripts/ColorManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+    private static Color[] colors =
+    {
+        Color.red,
+        Color.green,
+        Color.blue,
+        Color.yellow
+    };
+
+    public static Color GetRandomColor()
+    {
+        int randomInt = Random.Range(0, colors.Length);
+        return colors[randomInt];
+    }
+}
