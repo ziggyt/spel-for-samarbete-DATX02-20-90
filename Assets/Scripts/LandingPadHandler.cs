9353feb82b63390434a16dcb7280a404f1794c5b
Johan Lindkvist <johanlindkvist89@gmail.com>
2020-04-30 16:47:15 +0200

    Add ship land trigger + Start landing pad handler

---
+++ Assets/Scripts/LandingPadHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LandingPadHandler : NetworkBehaviour
+{
+    void Start()
+    {
+       // TODO: Init current and next pad and assign color via ColorCoordinator 
+    }
+
+    void Update()
+    {
+        
+    }
+}
