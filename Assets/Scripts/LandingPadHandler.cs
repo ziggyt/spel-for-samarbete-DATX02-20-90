6243075ef5ff874117559fbfb40c726fe1fe956b
Johan Lindkvist <johanlindkvist89@gmail.com>
2020-05-01 10:45:14 +0200

    Add random pad start colors with sync

--- Assets/Scripts/LandingPadHandler.cs
+++ Assets/Scripts/LandingPadHandler.cs
@@ -5,13 +5,36 @@ using UnityEngine.Networking;
 
 public class LandingPadHandler : NetworkBehaviour
 {
-    void Start()
+    // Variables
+    [SerializeField] private GameObject currentPad;
+    [SerializeField] private GameObject nextPad;
+    [SyncVar] private Color currentPadColor;
+    [SyncVar] private Color nextPadColor;
+
+    // Assigns the synced pad colors
+    private void AssignPadColors()
     {
-       // TODO: Init current and next pad and assign color via ColorCoordinator 
+        currentPad.GetComponent<MeshRenderer>().material.color = currentPadColor;
+        nextPad.GetComponent<MeshRenderer>().material.color = nextPadColor;
     }
 
-    void Update()
+    // Gets random pad colors and registers them
+    private void RegisterPadColors()
     {
-        
+        currentPadColor = ColorCoordinator.GetRandomColor();
+        nextPadColor = ColorCoordinator.GetRandomColor();
+        ColorCoordinator.RegisterPadColor(currentPadColor);
+        ColorCoordinator.RegisterPadColor(nextPadColor);
+    }
+
+    // Registers colors if server and assings pad start colors
+    void Start()
+    {
+        if (isServer)
+        {
+            RegisterPadColors();
+        }
+
+        AssignPadColors();
     }
 }
