ed16d275688d256aa465e92276f25d498ab6eccf
ziggyt <frmax@student.chalmers.se>
2020-05-04 19:37:15 +0200

    Add MainMenuScript for button functionality

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("Loaded Scene!");
+    }
+
+    public void ChangeSettings()
+    {
+        Debug.Log("Settings management is not implemented yet");
+    }
+}
