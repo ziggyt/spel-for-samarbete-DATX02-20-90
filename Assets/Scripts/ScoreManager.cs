0cb195441b0bdf60a5a2fc913e24b6de2b7f29b4
Camilla Söderlund <camsoder@student.chalmers.se>
2020-05-07 09:19:25 +0200

    Add ScoreManager script

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int scoreValue;
+    private Text scoreText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + scoreValue;
+    }
+}
