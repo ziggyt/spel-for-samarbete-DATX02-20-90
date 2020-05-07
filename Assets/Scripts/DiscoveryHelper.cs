71ea23f180061a9aa1e8c4ef618d8776bb212afb
ziggyt <frmax@student.chalmers.se>
2020-05-08 00:37:33 +0200

    Add working broadcast and host functionality

--- Assets/Scripts/DiscoveryHelper.cs
+++ Assets/Scripts/DiscoveryHelper.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.Networking;
+using Random = System.Random;
 
 
 public class DiscoveryHelper : NetworkDiscovery
 {
     bool hasFoundBroadcast = false;
     String serverIp = String.Empty;
-    
+
 
     private void Awake()
     {
         Initialize();
         StartAsClient();
+       // broadcastPort = 
+        showGUI = false;
+        broadcastInterval = 250;
+        //useNetworkManager = true; //todo om något går fel kolla denna
     }
 
     public override void OnReceivedBroadcast(string fromAddress, string data)
@@ -21,6 +26,8 @@ public class DiscoveryHelper : NetworkDiscovery
         serverIp = fromAddress;
         
         base.OnReceivedBroadcast(fromAddress, data);
+        
+        Debug.Log("Recieved broadcast from: " + fromAddress + " with data: " + data);
     }
     
     
