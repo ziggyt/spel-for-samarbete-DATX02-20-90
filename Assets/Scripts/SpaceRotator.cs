a53f011952cb8ba16123411ec5f8647f2a79d8dc
Johan Lindkvist <johanlindkvist89@gmail.com>
2020-05-07 15:32:45 +0200

    Add mothership and rotating space

---
+++ Assets/Scripts/SpaceRotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceRotator : MonoBehaviour
+{
+    [SerializeField] private float rotationsPerMinute = 10.0f;
+    void Update()
+    {
+
+        transform.Rotate(0, (float) 6.0 * rotationsPerMinute * Time.deltaTime, 0);
+    }
+}
