0e12d913d5bdee6fadee9c1078ebd2a69df2a9e7
ziggyt <frmax@student.chalmers.se>
2020-05-07 19:56:40 +0200

    Begin adding Network Discovery functionality

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,35 +1,65 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+
 
 public class MainMenuScript : MonoBehaviour
 {
+    public NetworkDiscovery _networkDiscovery;
+    public NetworkManager _networkManager;
     
-    //private ArrayList _networkComponents = new ArrayList();
+    private string IP;
+
+
+    private void Awake()
+    {
+        _networkDiscovery.Initialize();
+        _networkDiscovery.StartAsClient();
+        
+        Debug.Log("Discovery started");
+    }
 
     public void PlayGame()
     {
-        /*
-        _networkComponents.AddRange(GameObject.FindGameObjectsWithTag("NetworkComponent"));
         
-        Debug.Log(_networkComponents.Count);
+        IP = _networkManager.networkAddress;
+        Debug.Log(IP);
+        
+        _networkManager.StartHost();
 
-        foreach (GameObject networkComponent in _networkComponents)
-        {
-            networkComponent.SetActive(true);
-        }
-        */
-        
-        
-        GameObject.Find("Spawners").SetActive(true);
-        Debug.Log("Enabled ship spawner!");
         GameObject.Find("MainMenuCanvas").SetActive(false);
         Debug.Log("Closed main menu");
     }
-
+    
     public void ChangeSettings()
     {
         Debug.Log("Settings management is not implemented yet");
     }
+
+    void OnBroadCastRecieved()
+    {
+        
+        
+    }
 }
+
+
+//private ArrayList _networkComponents = new ArrayList();
+    
+/*
+  _networkComponents.AddRange(GameObject.FindGameObjectsWithTag("NetworkComponent"));
+  
+  Debug.Log(_networkComponents.Count);
+
+  foreach (GameObject networkComponent in _networkComponents)
+  {
+      networkComponent.SetActive(true);
+  }
+  
+  //        GameObject.Find("Spawners").SetActive(true);
+//        Debug.Log("Enabled ship spawner!");
+  */
