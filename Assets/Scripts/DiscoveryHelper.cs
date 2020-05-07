c414e76fbf903cb47addc46e38181921120e0989
ziggyt <frmax@student.chalmers.se>
2020-05-07 21:15:40 +0200

    Add DiscoveryHelper.cs

---
+++ Assets/Scripts/DiscoveryHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class DiscoveryHelper : NetworkDiscovery
+{
+    bool hasFoundBroadcast = false;
+    String serverIp = String.Empty;
+    
+
+    private void Awake()
+    {
+        Initialize();
+        StartAsClient();
+    }
+
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        hasFoundBroadcast = true;
+        serverIp = fromAddress;
+        
+        base.OnReceivedBroadcast(fromAddress, data);
+    }
+    
+    
+
+    public string ServerIp => serverIp;
+    public bool HasFoundBroadcast => hasFoundBroadcast;
+}
