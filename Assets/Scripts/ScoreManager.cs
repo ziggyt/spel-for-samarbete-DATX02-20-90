9c1d2ec8ff241ae9be2be756a527f48d0b2f10bb
Johan Lindkvist <johanlindkvist89@gmail.com>
2020-05-07 14:39:08 +0200

    Add lives and game over screen

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,16 +3,15 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
+    // Variables
     public static int scoreValue;
     private Text scoreText;
     
-    // Start is called before the first frame update
     void Start()
     {
         scoreText = GetComponent<Text>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         scoreText.text = "Score: " + scoreValue;
