9aecdf116d500678f61842172b7f056c55e2dccf
Johan Lindkvist <johanlindkvist89@gmail.com>
2020-05-09 15:13:18 +0200

    Add game over manager

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour
+{
+    [SerializeField] private Button playAgainButton;
+    [SerializeField] private Button quitButton;
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    private void Start()
+    {
+        playAgainButton.onClick.AddListener(PlayAgain);
+        quitButton.onClick.AddListener(Quit);
+    }
+}
