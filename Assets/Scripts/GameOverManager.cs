60c838e17bd89de699241566777e5d09687f74a2
Johan Lindkvist <johanlindkvist89@gmail.com>
2020-05-09 15:43:57 +0200

    Add game over button functionality

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -9,13 +10,21 @@ public class GameOverManager : MonoBehaviour
     [SerializeField] private Button playAgainButton;
     [SerializeField] private Button quitButton;
 
+    private void ShutDownNetwork()
+    {
+        Destroy(NetworkManager.singleton.gameObject);
+        NetworkManager.Shutdown();
+    }
+
     public void PlayAgain()
     {
+        ShutDownNetwork();
         SceneManager.LoadScene(0);
     }
 
     public void Quit()
     {
+        ShutDownNetwork();
         Application.Quit();
     }
 
