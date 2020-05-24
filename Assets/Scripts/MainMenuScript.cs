26dc2b173ce7fa15f418b3825ccb366a4d1bb41d
ziggyt <frmax@student.chalmers.se>
2020-05-24 12:00:27 +0200

    Clean up commented debug statements

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -19,7 +19,6 @@ public class MainMenuScript : MonoBehaviour
     [FormerlySerializedAs("audio")] public AudioSource menuAudioSource;
     [FormerlySerializedAs("gameMusic")] public AudioSource gameAudioSource;
     [FormerlySerializedAs("audioImage")] public Image muteButtonBackgroundImage;
-    //public GameObject connectionInfo;
 
 
     private void Awake()
@@ -34,29 +33,24 @@ public class MainMenuScript : MonoBehaviour
     
     private void Update()
     {
-        //hasFoundHostInfo.GetComponent<Text>().text = "HasFoundBroadcast: " + _discoveryHelper.HasFoundBroadcast + "\nHasConnected: " + _hasConnected;
-
+        
         if (!_discoveryHelper.HasFoundBroadcast && !_discoveryHelper.running)
         {
             _discoveryHelper.initDiscoveryHelper();
         }
-
-        //Debug.Log("Discovery helper: " + _discoveryHelper.running);
+        
 
         if (_discoveryHelper.HasFoundBroadcast && !_hasConnected)
         {
             _discoveryHelper.StopAllCoroutines();
             _discoveryHelper.StopBroadcast();
-
-            //Debug.Log("Found host!");
+            
             String IP = _discoveryHelper.ServerIp;
-
-            //connectionInfo.GetComponent<Text>().text = "Found host: " + IP;
+            
             _networkManager.networkAddress = IP;
             _networkManager.networkPort = 7777;
             _networkManager.StartClient();
-
-            //Debug.Log("Connected to " + IP);
+            
             _hasConnected = true;
             
             buttonBackground.color = Color.cyan;
@@ -78,17 +72,13 @@ public class MainMenuScript : MonoBehaviour
             menuAudioSource.enabled = false;
             gameAudioSource.enabled = true;
             gameAudioSource.mute = menuAudioSource.mute;
-
-            //connectionInfo.GetComponent<Text>().text = "Started host";
-            //NetworkServer.Reset();
+            
             _discoveryHelper.StopBroadcast();
 
             _networkManager.StartHost();
-            //Debug.Log("Started host server");
+            
             _discoveryHelper.StartAsServer();
-
-
-            //Debug.Log("Started broadcast on " + _networkManager.networkAddress + ":" + _discoveryHelper.broadcastPort);
+            
         }
 
         GameObject[] uiComponents = GameObject.FindGameObjectsWithTag("MenuUIComponent");
@@ -97,8 +87,7 @@ public class MainMenuScript : MonoBehaviour
         {
             uiComponent.SetActive(false);
         }
-
-        //Debug.Log("Closed main menu");
+        
     }
 
 
